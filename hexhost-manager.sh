05d9964d9a2697f1bf8df6af23641841eda23e25
HexHost • IMMU <ritikchaudhary48088@gmail.com>
2026-06-17 15:17:04 +0530

    Update hexhost-manager.sh

--- hexhost-manager.sh
+++ hexhost-manager.sh
@@ -67,8 +67,8 @@ banner() {
     echo "  ██║  ██║███████╗██╔╝ ██╗"
     echo "  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝"
     echo -e "${RESET}"
-    echo -e "       ${WHITE}${BOLD}HEXHOST MANAGER TOOL${RESET}"
-    echo -e "       ${GRAY}${BOLD}ᴍᴀᴅᴇ ʙʏ : ɪᴍᴍᴜ${RESET}"
+    echo -e "          ${WHITE}${BOLD}HEXHOST MANAGER TOOL${RESET}"
+    echo -e "             ${GRAY}${BOLD}MADE BY : IMMU${RESET}"
     echo -e "${RED}${BOLD}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
   else
     echo -e "${RED}${BOLD}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
@@ -80,8 +80,8 @@ banner() {
     echo "  ██║  ██║███████╗██╔╝ ██╗██║  ██║╚██████╔╝███████║   ██║   "
     echo "  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   "
     echo -e "${RESET}"
-    echo -e "             ${WHITE}${BOLD}🚀 HEXHOST MANAGER TOOL${RESET}"
-    echo -e "                  ${WHITE}${BOLD}ᴍᴀᴅᴇ ʙʏ : ɪᴍᴍᴜ${RESET}"
+    echo -e "                ${WHITE}${BOLD}🚀 HEXHOST MANAGER TOOL${RESET}"
+    echo -e "                     ${WHITE}${BOLD}MADE BY : IMMU${RESET}"
     echo -e "${RED}${BOLD}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${RESET}"
   fi
 
